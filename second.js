07e9f6cd7e82c6e0e30617903bc0e61ea7f75ae0
motarek21 <125159078+motarek21@users.noreply.github.com>
2023-03-22 16:02:02 +0200

    Add files via upload

--- second.js
+++ second.js
@@ -1,110 +1,110 @@
 function button1() {
     var image = document.getElementById("cv1-image");
-    image.src = "Images/cv1/cv1-black.png";
+    image.src = "cv1-black.png";
     var link = document.getElementById("cv1-link");
-    link.href="cvs/cv1/cv1-black/form-1.html"
+    link.href="form-1.html"
   }
   function button2() {
     var image = document.getElementById("cv1-image");
-    image.src = "Images/cv1/cv1-blue.png";
+    image.src = "cv1-blue.png";
     var link = document.getElementById("cv1-link");
-    link.href="cvs/cv1/cv1-blue/form-1.html"
+    link.href="form-1.html"
   }
   function button3() {
     var image = document.getElementById("cv1-image");
-    image.src = "Images/cv1/cv1-gray.png";
+    image.src = "cv1-gray.png";
     var link = document.getElementById("cv1-link");
-    link.href="cvs/cv1/cv1-gray/form-1.html"
+    link.href="form-1.html"
   }
   function button4() {
     var image = document.getElementById("cv1-image");
-    image.src = "Images/cv1/cv1-brown.png";
+    image.src = "cv1-brown.png";
     var link = document.getElementById("cv1-link");
-    link.href="cvs/cv1/cv1-brown/form-1.html"
+    link.href="form-1.html"
   }
   function button5() {
     var image = document.getElementById("cv1-image");
-    image.src = "Images/cv1/cv1-olive.png";
+    image.src = "cv1-olive.png";
     var link = document.getElementById("cv1-link");
-    link.href="cvs/cv1/cv1-olive/form-1.html"
+    link.href="form-1.html"
   }
   function button6() {
     var image = document.getElementById("cv1-image");
-    image.src = "Images/cv1/cv1-orange.png";
+    image.src = "cv1-orange.png";
     var link = document.getElementById("cv1-link");
-    link.href="cvs/cv1/cv1-orange/form-1.html"
+    link.href="form-1.html"
   }
   function button7() {
     var image = document.getElementById("cv2-image");
-    image.src = "Images/cv2/Second-Template-black-.png";
+    image.src = "Second-Template-black-.png";
     var link = document.getElementById("cv2-link");
-    link.href="cvs/cv2/cv2-black/form-1-cv2.html"
+    link.href="form-1-cv2.html"
   }
   function button8() {
     var image = document.getElementById("cv2-image");
-    image.src = "Images/cv2/Second-Template-blue.png";
+    image.src = "Second-Template-blue.png";
     var link = document.getElementById("cv2-link");
-    link.href="cvs/cv2/cv2-blue/form-1-cv2.html"
+    link.href="form-1-cv2.html"
   }
   function button9() {
     var image = document.getElementById("cv2-image");
-    image.src = "Images/cv2/Second-Template-gray.png";
+    image.src = "Second-Template-gray.png";
     var link = document.getElementById("cv2-link");
-    link.href="cvs/cv2/cv2-gray/form-1-cv2.html"
+    link.href="form-1-cv2.html"
   }
   function button10() {
     var image = document.getElementById("cv2-image");
-    image.src = "Images/cv2/Second-Template-brown.png";
+    image.src = "Second-Template-brown.png";
     var link = document.getElementById("cv2-link");
-    link.href="cvs/cv2/cv2-brown/form-1-cv2.html"
+    link.href="form-1-cv2.html"
   }
   function button11() {
     var image = document.getElementById("cv2-image");
-    image.src = "Images/cv2/Second-Template-olive.png";
+    image.src = "Second-Template-olive.png";
     var link = document.getElementById("cv2-link");
-    link.href="cvs/cv2/cv2-olive/form-1-cv2.html"
+    link.href="form-1-cv2.html"
   }
   function button12() {
     var image = document.getElementById("cv2-image");
-    image.src = "Images/cv2/Second-Template-orange.png";
+    image.src = "Second-Template-orange.png";
     var link = document.getElementById("cv2-link");
-    link.href="cvs/cv2/cv2-orange/form-1-cv2.html"
+    link.href="form-1-cv2.html"
   }
   function button13() {
     var image = document.getElementById("cv3-image");
-    image.src = "Images/cv3/third-tempelate-black-.png";
+    image.src = "third-tempelate-black-.png";
     var link = document.getElementById("cv3-link");
-    link.href="cvs/cv3/cv3-black/form-1-cv3.html"
+    link.href="form-1-cv3.html"
   }
   function button14() {
     var image = document.getElementById("cv3-image");
-    image.src = "Images/cv3/third-tempelate-blue-.png";
+    image.src = "third-tempelate-blue-.png";
     var link = document.getElementById("cv3-link");
-    link.href="cvs/cv3/cv3-blue/form-1-cv3.html"
+    link.href="form-1-cv3.html"
   }
   function button15() {
     var image = document.getElementById("cv3-image");
-    image.src ="Images/cv3/third-tempelate-gray.png";
+    image.src ="third-tempelate-gray.png";
     var link = document.getElementById("cv3-link");
-    link.href="cvs/cv3/cv3-gray/form-1-cv3.html"
+    link.href="form-1-cv3.html"
   }
   function button16() {
     var image = document.getElementById("cv3-image");
-    image.src = "Images/cv3/third-tempelate-brown-.png";
+    image.src = "third-tempelate-brown-.png";
     var link = document.getElementById("cv3-link");
-    link.href="cvs/cv3/cv3-brown/form-1-cv3.html"
+    link.href="form-1-cv3.html"
   }
   function button17() {
     var image = document.getElementById("cv3-image");
-    image.src = "Images/cv3/third-tempelate-olive.png";
+    image.src = "third-tempelate-olive.png";
     var link = document.getElementById("cv3-link");
-    link.href="cvs/cv3/cv3-olive/form-1-cv3.html"
+    link.href="form-1-cv3.html"
   }
   function button18() {
     var image = document.getElementById("cv3-image");
-    image.src = "Images/cv3/third-tempelate-orange.png";
+    image.src = "third-tempelate-orange.png";
     var link = document.getElementById("cv3-link");
-    link.href="cvs/cv3/cv3-orange/form-1-cv3.html"
+    link.href="form-1-cv3.html"
   }
   document.querySelector(".scroll-top-btn").addEventListener("click", function() {
     window.scrollTo(0, 0);
