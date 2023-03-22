dda6faac76f9a2f761272d46dfabcc5c85cfa418
motarek21 <125159078+motarek21@users.noreply.github.com>
2023-03-22 16:31:49 +0200

    Add files via upload

---
+++ second.js
@@ -0,0 +1,121 @@
+function button1() {
+    var image = document.getElementById("cv1-image");
+    image.src = "cv1-black.png";
+    var link = document.getElementById("cv1-link");
+    link.href="form-1.html"
+  }
+  function button2() {
+    var image = document.getElementById("cv1-image");
+    image.src = "cv1-blue.png";
+    var link = document.getElementById("cv1-link");
+    link.href="form-1.html"
+  }
+  function button3() {
+    var image = document.getElementById("cv1-image");
+    image.src = "cv1-gray.png";
+    var link = document.getElementById("cv1-link");
+    link.href="form-1.html"
+  }
+  function button4() {
+    var image = document.getElementById("cv1-image");
+    image.src = "cv1-brown.png";
+    var link = document.getElementById("cv1-link");
+    link.href="form-1.html"
+  }
+  function button5() {
+    var image = document.getElementById("cv1-image");
+    image.src = "cv1-olive.png";
+    var link = document.getElementById("cv1-link");
+    link.href="form-1.html"
+  }
+  function button6() {
+    var image = document.getElementById("cv1-image");
+    image.src = "cv1-orange.png";
+    var link = document.getElementById("cv1-link");
+    link.href="form-1.html"
+  }
+  function button7() {
+    var image = document.getElementById("cv2-image");
+    image.src = "Second-Template-black-.png";
+    var link = document.getElementById("cv2-link");
+    link.href="form-1-cv2.html"
+  }
+  function button8() {
+    var image = document.getElementById("cv2-image");
+    image.src = "Second-Template-blue.png";
+    var link = document.getElementById("cv2-link");
+    link.href="form-1-cv2.html"
+  }
+  function button9() {
+    var image = document.getElementById("cv2-image");
+    image.src = "Second-Template-gray.png";
+    var link = document.getElementById("cv2-link");
+    link.href="form-1-cv2.html"
+  }
+  function button10() {
+    var image = document.getElementById("cv2-image");
+    image.src = "Second-Template-brown.png";
+    var link = document.getElementById("cv2-link");
+    link.href="form-1-cv2.html"
+  }
+  function button11() {
+    var image = document.getElementById("cv2-image");
+    image.src = "Second-Template-olive.png";
+    var link = document.getElementById("cv2-link");
+    link.href="form-1-cv2.html"
+  }
+  function button12() {
+    var image = document.getElementById("cv2-image");
+    image.src = "Second-Template-orange.png";
+    var link = document.getElementById("cv2-link");
+    link.href="form-1-cv2.html"
+  }
+  function button13() {
+    var image = document.getElementById("cv3-image");
+    image.src = "third-tempelate-black-.png";
+    var link = document.getElementById("cv3-link");
+    link.href="form-1-cv3.html"
+  }
+  function button14() {
+    var image = document.getElementById("cv3-image");
+    image.src = "third-tempelate-blue-.png";
+    var link = document.getElementById("cv3-link");
+    link.href="form-1-cv3.html"
+  }
+  function button15() {
+    var image = document.getElementById("cv3-image");
+    image.src ="third-tempelate-gray.png";
+    var link = document.getElementById("cv3-link");
+    link.href="form-1-cv3.html"
+  }
+  function button16() {
+    var image = document.getElementById("cv3-image");
+    image.src = "third-tempelate-brown-.png";
+    var link = document.getElementById("cv3-link");
+    link.href="form-1-cv3.html"
+  }
+  function button17() {
+    var image = document.getElementById("cv3-image");
+    image.src = "third-tempelate-olive.png";
+    var link = document.getElementById("cv3-link");
+    link.href="form-1-cv3.html"
+  }
+  function button18() {
+    var image = document.getElementById("cv3-image");
+    image.src = "third-tempelate-orange.png";
+    var link = document.getElementById("cv3-link");
+    link.href="form-1-cv3.html"
+  }
+  document.querySelector(".scroll-top-btn").addEventListener("click", function() {
+    window.scrollTo(0, 0);
+  });
+
+
+  var modal = document.getElementById("myModal");
+  var closeBtn = document.getElementsByClassName("close")[0];
+  
+  modal.style.display = "none";
+  
+  closeBtn.onclick = function() {
+    modal.style.display = "none";
+  }
